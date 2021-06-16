5b2b93149970e35c5d5f75d038d98dbded132d97
imoempire <mrimoempire@gmail.com>
2021-06-16 16:31:23 +0000

    addd Blog-List component

---
+++ src/Component/BlogList.js
@@ -0,0 +1,15 @@
+const BlogList = ({blogs, title}) => {
+    return (  
+       <div className="Blog-list">
+           <h1> {title}</h1>
+            {blogs.map( (blogs) => (
+            <div className="blog-preview">
+                <h1>{blogs.title}</h1>
+                <p>by {blogs.author}</p>
+            </div>
+        ))}
+       </div>
+    );
+}
+ 
+export default BlogList;
