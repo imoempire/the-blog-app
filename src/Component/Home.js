edf121f5b2c76498fa98e263734a12278dc5ca7a
imoempire <mrimoempire@gmail.com>
2021-06-18 15:29:49 +0000

    add new code

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,21 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlog] = useState( [
-      {title: 'MY First BLOG', body: ' lorem jjdjbuhjvuvuviuvuivd...', author: 'Isaac', id:1},
-      {title: 'T-THE BLOG', body: 'T-THE is about know you know and what you know', author: 'James', id:2},
-      {title: 'MY First BLOG', body: ' The is a story about THe new james Bond we know.....', author: 'IMark', id:3}]);
-
+  const [blogs, setBlog] = useState(null)
 const handDelete =(id) =>{
     const newBlogs =blogs.filter(blogs => blogs.id !== id);
     setBlog(newBlogs);
 }
 
+useEffect( ()=> {
+    fetch("http://localhost:8000/blogs")
+    .then( res => {
+        return res.json()
+        .then(data => {
+            setBlog(data);
+        })
+    })
+} );
     return (
         <div className="Home">
-        <BlogList blogs={blogs} title='ALL BOLGS!' handDelete={handDelete}/>
-        </div>
+{ blogs && <BlogList blogs={blogs} title='ALL BOLGS!' handDelete={handDelete}/>
+}        </div>
     );
 }
  
