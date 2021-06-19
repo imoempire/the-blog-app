3169f321d2e96e870921d42e564ee623d1305d4e
imoempire <mrimoempire@gmail.com>
2021-06-19 10:53:16 +0000

    add error message

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -2,21 +2,29 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlog] = useState(null)
-  const [isPending, setIsPending]= useState(true)
+  const [blogs, setBlog] = useState(null);
+  const [isPending, setIsPending]= useState(true);
+  const [error, setError]= useState(null);
 
 useEffect( ()=> {
     fetch(" http://localhost:7000/blogs")
     .then( res => {
+        if(!res.ok) {
+            throw Error("Could not get data")
+        } 
         return res.json()
         .then(data => {
             setBlog(data);
             setIsPending(false);
         })
+        .catch(err => {
+            setError(err.message);
+        })
     })
 }, []);
     return (
         <div className="Home">
+            { error && <div> {error} </div>  }
             { isPending && <div>Loading...</div> }
 { blogs && <BlogList blogs={blogs} title='ALL BOLGS!' />
 }        
